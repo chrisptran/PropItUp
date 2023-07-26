6495342ea757bbdbe8bcd1cb019b69525683051a
chrisptran <christopher.p.tran@gmail.com>
2023-07-26 15:17:38 -0700

    Second commit

--- src/components/PersonCard.jsx
+++ src/components/PersonCard.jsx
@@ -1,13 +1,26 @@
 import React, { Component } from 'react'
 
 export class PersonCard extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            age: this.props.age
+        }
+    }
+
     render() {
-        const {lastName, firstName, age, hairColor} = this.props;
+        const {lastName, firstName, hairColor} = this.props;
         return (
             <div>
                 <h1>{lastName}, {firstName}</h1>
-                <p>Age: {age}</p>
+                <p>Age: {this.state.age}</p>
                 <p>Hair Color: {hairColor}</p>
+            <button onClick={ () => {
+                this.setState({
+                    age: this.state.age+1
+                })
+            }}>Birthday {this.props.firstName}</button>
             </div>
         )
     }
